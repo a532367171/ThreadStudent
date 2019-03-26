a906377b3f6d53c8edcefec500c558909277ba06
a53236717171 <532367171@qq.com>
2019-03-26 20:13:31 +0800

    添加项目文件。

---
+++ Chapter1/Recipe2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace Chapter1.Recipe2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Thread t = new Thread(PrintNumbersWithDelay);
+			t.Start();
+			PrintNumbers();
+            Console.ReadLine();
+
+        }
+
+        static void PrintNumbers()
+		{
+			WriteLine("Starting...");
+			for (int i = 1; i < 10; i++)
+			{
+				WriteLine(i);
+			}
+		}
+
+		static void PrintNumbersWithDelay()
+		{
+			WriteLine("Starting...");
+			for (int i = 1; i < 10; i++)
+			{
+				Sleep(TimeSpan.FromSeconds(2));
+				WriteLine(i);
+			}
+		}
+	}
+}
